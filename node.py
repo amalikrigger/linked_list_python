220a38a0942e738ca6918bc36afb0a0f73abbedb
amalikrigger <amalikrigger93@gmail.com>
2025-04-14 06:59:06 -0400

    Optimized Node Class

--- node.py
+++ node.py
@@ -1,9 +1,9 @@
+from typing import Any, Optional
+
 class Node:
-    def __init__(self, value, next = None):
+    def __init__(self, value: Any, next: Optional['Node'] = None):
         self.value = value
         self.next = next
 
-
-
-
-        
+    def __repr__(self):
+        return f"Node({self.value})"
